152a2cdac66d6506cbdf25d70a241f863e160b9c
liushuaiyang <a_liushuaiyang@163.com>
2026-06-24 21:44:27 +0800

    init project

---
+++ src/main/java/org/example/sopanalysisagent/SopAnalysisAgentApplication.java
@@ -0,0 +1,13 @@
+package org.example.sopanalysisagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopAnalysisAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopAnalysisAgentApplication.class, args);
+    }
+
+}
